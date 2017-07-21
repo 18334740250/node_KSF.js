d769339a12da9068f6400a2686cd7f375ae4a959
18334740250 <584818348@qq.com>
2017-07-21 20:16:32 +0800

    Create crawler.js

---
+++ crawler.js
@@ -0,0 +1,70 @@
+//抓取网页的小爬虫
+var http =require('http');
+var cheerio=require('cheerio');
+var url='http://www.imooc.com/learn/348';
+
+function filterChapters(html){
+	var $=cheerio.load(html);
+	
+	var chapters=$('.chapter');
+	
+	var courseData=[];
+	
+	chapters.each(function(item){
+		var chapter=$(this);
+		
+		var chapterTitle=chapter.find('strong').text();
+		
+		var videos=chapter.find('.video').children('li');
+		
+		var chapterData={
+			chapterTitle:chapterTitle,	
+			videos:[]
+		}
+		
+		videos.each(function(item){
+			var video=$(this).find('.J-media-item');
+			var videoTitle=video.text();
+			var id=video.attr('href').split('video/')[1];
+			
+			chapterData.videos.push({
+				title:videoTitle,
+				id:id				
+			})
+			
+		})
+		courseData.push(chapterData);
+	})
+	return courseData;
+}
+
+
+function printcourseInfo(courseData){
+	courseData.forEach(function(item){
+		var chapterTitle=item.chapterTitle;
+		console.log(chapterTitle+'\n');
+		item.videos.forEach(function (item){
+			var title=item.title;
+			var id=item.id;
+			console.log('['+id+']'+title+'\n');
+			
+		})
+		
+	})
+	
+	
+	
+}
+
+http.get(url,function(res){
+	var html='';
+	res.on('data',function(data){
+		html+=data;
+	})
+	res.on('end',function(){
+	var courseData=filterChapters(html);
+	printcourseInfo(courseData);
+	})
+		}).on('error',function(){
+	console.log('获取代码失败');
+});
